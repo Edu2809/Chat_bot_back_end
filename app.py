8085d00addb1502c7550b27e293f6cfe52317245
Edu2809 <eduardolima2809@gmail.com>
2025-10-21 20:16:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -228,3 +228,4 @@ if __name__ == "__main__":
     # Define a porta, usando a variável de ambiente PORT se existir, caso contrário, usa 10000
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
